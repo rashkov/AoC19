9ac92e6cf20232a1ce098060bff3bcb75fa2bbd4
Michael Rashkovsky <mrashkovsky@gmail.com>
2019-12-11 15:50:58 -0500

    WIP day 3 part 2

--- src/day3/Day3.bs.js
+++ src/day3/Day3.bs.js
@@ -5,12 +5,11 @@ var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 var Caml_obj = require("bs-platform/lib/js/caml_obj.js");
 var Belt_List = require("bs-platform/lib/js/belt_List.js");
-var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
 var Caml_format = require("bs-platform/lib/js/caml_format.js");
 var Caml_string = require("bs-platform/lib/js/caml_string.js");
+var Caml_primitive = require("bs-platform/lib/js/caml_primitive.js");
 var Caml_exceptions = require("bs-platform/lib/js/caml_exceptions.js");
 var Caml_builtin_exceptions = require("bs-platform/lib/js/caml_builtin_exceptions.js");
-var Input3$ReasonReactExamples = require("./Input3.bs.js");
 
 var UnknownDirectionInput = Caml_exceptions.create("Day3-ReasonReactExamples.UnknownDirectionInput");
 
@@ -24,29 +23,54 @@ function tag_coords_with_line_id(coords, line_id) {
 }
 
 function get_position_after_move(start_coord, move) {
+  var steps = start_coord[/* steps */2] + move[/* distance */1] | 0;
   var match = move[/* direction */0];
   switch (match) {
     case /* Up */0 :
         return /* record */[
                 /* x */start_coord[/* x */0],
-                /* y */start_coord[/* y */1] + move[/* distance */1] | 0
+                /* y */start_coord[/* y */1] + move[/* distance */1] | 0,
+                /* steps */steps
               ];
     case /* Down */1 :
         return /* record */[
                 /* x */start_coord[/* x */0],
-                /* y */start_coord[/* y */1] - move[/* distance */1] | 0
+                /* y */start_coord[/* y */1] - move[/* distance */1] | 0,
+                /* steps */steps
               ];
     case /* Left */2 :
         return /* record */[
                 /* x */start_coord[/* x */0] - move[/* distance */1] | 0,
-                /* y */start_coord[/* y */1]
+                /* y */start_coord[/* y */1],
+                /* steps */steps
               ];
     case /* Right */3 :
         return /* record */[
                 /* x */start_coord[/* x */0] + move[/* distance */1] | 0,
-                /* y */start_coord[/* y */1]
+                /* y */start_coord[/* y */1],
+                /* steps */steps
               ];
     
+  }
+}
+
+function range(a, b) {
+  var r = a - b | 0;
+  if (r === 0) {
+    return /* :: */[
+            b,
+            /* [] */0
+          ];
+  } else if (r < 0) {
+    return /* :: */[
+            a,
+            range(a + 1 | 0, b)
+          ];
+  } else {
+    return /* :: */[
+            a,
+            range(a - 1 | 0, b)
+          ];
   }
 }
 
@@ -56,37 +80,41 @@ function map_move_to_coords(start_coord, move) {
     case /* Up */0 :
         var start = start_coord[/* y */1] + 1 | 0;
         var stop = start_coord[/* y */1] + move[/* distance */1] | 0;
-        return Belt_List.map(Belt_List.fromArray(Belt_Array.range(start < stop ? start : stop, start > stop ? start : stop)), (function (y) {
+        return Belt_List.mapWithIndex(range(start, stop), (function (idx, y) {
                       return /* record */[
                               /* x */start_coord[/* x */0],
-                              /* y */y
+                              /* y */y,
+                              /* steps */(start_coord[/* steps */2] + idx | 0) + 1 | 0
                             ];
                     }));
     case /* Down */1 :
         var start$1 = start_coord[/* y */1] - 1 | 0;
         var stop$1 = start_coord[/* y */1] - move[/* distance */1] | 0;
-        return Belt_List.map(Belt_List.fromArray(Belt_Array.range(start$1 < stop$1 ? start$1 : stop$1, start$1 > stop$1 ? start$1 : stop$1)), (function (y) {
+        return Belt_List.mapWithIndex(range(start$1, stop$1), (function (idx, y) {
                       return /* record */[
                               /* x */start_coord[/* x */0],
-                              /* y */y
+                              /* y */y,
+                              /* steps */(start_coord[/* steps */2] + idx | 0) + 1 | 0
                             ];
                     }));
     case /* Left */2 :
         var start$2 = start_coord[/* x */0] - 1 | 0;
         var stop$2 = start_coord[/* x */0] - move[/* distance */1] | 0;
-        return Belt_List.map(Belt_List.fromArray(Belt_Array.range(start$2 < stop$2 ? start$2 : stop$2, start$2 > stop$2 ? start$2 : stop$2)), (function (x) {
+        return Belt_List.mapWithIndex(range(start$2, stop$2), (function (idx, x) {
                       return /* record */[
                               /* x */x,
-                              /* y */start_coord[/* y */1]
+                              /* y */start_coord[/* y */1],
+                              /* steps */(start_coord[/* steps */2] + idx | 0) + 1 | 0
                             ];
                     }));
     case /* Right */3 :
         var start$3 = start_coord[/* x */0] + 1 | 0;
         var stop$3 = start_coord[/* x */0] + move[/* distance */1] | 0;
-        return Belt_List.map(Belt_List.fromArray(Belt_Array.range(start$3 < stop$3 ? start$3 : stop$3, start$3 > stop$3 ? start$3 : stop$3)), (function (x) {
+        return Belt_List.mapWithIndex(range(start$3, stop$3), (function (idx, x) {
                       return /* record */[
                               /* x */x,
-                              /* y */start_coord[/* y */1]
+                              /* y */start_coord[/* y */1],
+                              /* steps */(start_coord[/* steps */2] + idx | 0) + 1 | 0
                             ];
                     }));
     
@@ -97,7 +125,8 @@ function map_move_list_to_coords(moves) {
   return Belt_List.reduce(moves, /* tuple */[
                 /* record */[
                   /* x */0,
-                  /* y */0
+                  /* y */0,
+                  /* steps */0
                 ],
                 /* [] */0
               ], (function (param, move) {
@@ -162,15 +191,19 @@ var CoordMap = $$Map.Make({
       compare: compare
     });
 
-var line1_tagged_coords = tag_coords_with_line_id(map_move_list_to_coords(map_input_to_moves_list(Input3$ReasonReactExamples.line1_str)), 1);
+var line1_input = "R8,U5,L5,D3";
 
-var line2_tagged_coords = tag_coords_with_line_id(map_move_list_to_coords(map_input_to_moves_list(Input3$ReasonReactExamples.line2_str)), 2);
+var line2_input = "U7,R6,D4,L4";
+
+var line1_tagged_coords = tag_coords_with_line_id(map_move_list_to_coords(map_input_to_moves_list(line1_input)), 1);
+
+var line2_tagged_coords = tag_coords_with_line_id(map_move_list_to_coords(map_input_to_moves_list(line2_input)), 2);
 
 var all_tagged_coords = Belt_List.concat(line1_tagged_coords, line2_tagged_coords);
 
-function does_collide(querying_line_id, line_ids) {
-  return Belt_List.some(line_ids, (function (line_id) {
-                return Caml_obj.caml_notequal(line_id, querying_line_id);
+function does_collide(querying_line_id, touches) {
+  return Belt_List.some(touches, (function (param) {
+                return param[0] !== querying_line_id;
               }));
 }
 
@@ -182,9 +215,9 @@ var match = Belt_List.reduce(all_tagged_coords, /* tuple */[
         var coord_map = acc[0];
         var line_id = param[1];
         var coord = param[0];
-        var line_ids;
+        var touches;
         try {
-          line_ids = Curry._2(CoordMap.find, /* tuple */[
+          touches = Curry._2(CoordMap.find, /* tuple */[
                 coord[/* x */0],
                 coord[/* y */1]
               ], coord_map);
@@ -195,7 +228,10 @@ var match = Belt_List.reduce(all_tagged_coords, /* tuple */[
                   coord[/* x */0],
                   coord[/* y */1]
                 ], /* :: */[
-                  line_id,
+                  /* tuple */[
+                    line_id,
+                    coord[/* steps */2]
+                  ],
                   /* [] */0
                 ], coord_map);
             return /* tuple */[
@@ -206,14 +242,16 @@ var match = Belt_List.reduce(all_tagged_coords, /* tuple */[
             throw exn;
           }
         }
-        var match = does_collide(line_id, line_ids);
-        if (match) {
+        if (does_collide(line_id, touches)) {
           var new_map$1 = Curry._3(CoordMap.add, /* tuple */[
                 coord[/* x */0],
                 coord[/* y */1]
               ], /* :: */[
-                line_id,
-                line_ids
+                /* tuple */[
+                  line_id,
+                  coord[/* steps */2]
+                ],
+                touches
               ], coord_map);
           return /* tuple */[
                   new_map$1,
@@ -223,8 +261,18 @@ var match = Belt_List.reduce(all_tagged_coords, /* tuple */[
                   ]
                 ];
         } else {
+          var new_map$2 = Curry._3(CoordMap.add, /* tuple */[
+                coord[/* x */0],
+                coord[/* y */1]
+              ], /* :: */[
+                /* tuple */[
+                  line_id,
+                  coord[/* steps */2]
+                ],
+                touches
+              ], coord_map);
           return /* tuple */[
-                  coord_map,
+                  new_map$2,
                   collisions
                 ];
         }
@@ -232,21 +280,23 @@ var match = Belt_List.reduce(all_tagged_coords, /* tuple */[
 
 var collisions = match[1];
 
-var closest_collision = Belt_List.head(Belt_List.sort(Belt_List.map(collisions, (function (collision_coord) {
+var closest_collision_by_manhattan_distance = Belt_List.head(Belt_List.sort(Belt_List.map(collisions, (function (collision_coord) {
                 return collision_coord[/* x */0] + collision_coord[/* y */1] | 0;
               })), (function (a, b) {
             return a - b | 0;
           })));
 
-var collision_output = closest_collision !== undefined ? "Closest collision has manhattan distance: " + String(closest_collision) : "No collisions";
+var collision_output_manhattan_distance = closest_collision_by_manhattan_distance !== undefined ? "Closest collision has manhattan distance: " + String(closest_collision_by_manhattan_distance) : "No collisions";
+
+var closest_collision_by_steps = Belt_List.head(Belt_List.sort(collisions, (function (a, b) {
+            return Caml_primitive.caml_int_compare(a[/* steps */2], b[/* steps */2]);
+          })));
+
+var collision_output_steps = closest_collision_by_steps !== undefined ? "Closest collision by number of steps: " + String(closest_collision_by_steps[/* steps */2]) : "No collisions";
 
 function Day3(Props) {
-  return React.createElement("div", undefined, collision_output);
+  return React.createElement("div", undefined, React.createElement("div", undefined, collision_output_steps), React.createElement("div", undefined, collision_output_manhattan_distance));
 }
-
-var line1_input = Input3$ReasonReactExamples.line1_str;
-
-var line2_input = Input3$ReasonReactExamples.line2_str;
 
 var coord_map = match[0];
 
@@ -255,6 +305,7 @@ var make = Day3;
 exports.UnknownDirectionInput = UnknownDirectionInput;
 exports.tag_coords_with_line_id = tag_coords_with_line_id;
 exports.get_position_after_move = get_position_after_move;
+exports.range = range;
 exports.map_move_to_coords = map_move_to_coords;
 exports.map_move_list_to_coords = map_move_list_to_coords;
 exports.map_str_to_move = map_str_to_move;
@@ -268,7 +319,9 @@ exports.all_tagged_coords = all_tagged_coords;
 exports.does_collide = does_collide;
 exports.coord_map = coord_map;
 exports.collisions = collisions;
-exports.closest_collision = closest_collision;
-exports.collision_output = collision_output;
+exports.closest_collision_by_manhattan_distance = closest_collision_by_manhattan_distance;
+exports.collision_output_manhattan_distance = collision_output_manhattan_distance;
+exports.closest_collision_by_steps = closest_collision_by_steps;
+exports.collision_output_steps = collision_output_steps;
 exports.make = make;
 /* CoordMap Not a pure module */
--- src/day3/Day3.re
+++ src/day3/Day3.re
@@ -10,19 +10,32 @@ type move = {
 type coord = {
   x: int,
   y: int,
+  steps: int /* Number of steps taken to reach this position */
 };
 type line_id = int;
+type touches = list((line_id, coord));
 exception UnknownDirectionInput;
 
 let tag_coords_with_line_id = (coords: list(coord), line_id: line_id) => {
   coords->Belt.List.map(coord => (coord, line_id));
 };
 let get_position_after_move = (start_coord: coord, move: move) => {
+  let steps = start_coord.steps + move.distance;
   switch (move.direction) {
-  | Right => {x: start_coord.x + move.distance, y: start_coord.y}
-  | Left => {x: start_coord.x - move.distance, y: start_coord.y}
-  | Up => {x: start_coord.x, y: start_coord.y + move.distance}
-  | Down => {x: start_coord.x, y: start_coord.y - move.distance}
+  | Right => {x: start_coord.x + move.distance, y: start_coord.y, steps}
+  | Left => {x: start_coord.x - move.distance, y: start_coord.y, steps}
+  | Up => {x: start_coord.x, y: start_coord.y + move.distance, steps}
+  | Down => {x: start_coord.x, y: start_coord.y - move.distance, steps}
+  };
+};
+let rec range = (a, b) => {
+  let r = a - b;
+  if (r == 0) {
+    [b];
+  } else if (r < 0) {
+    [a, ...range(a + 1, b)];
+  } else {
+    [a, ...range(a - 1, b)];
   };
 };
 let map_move_to_coords = (start_coord: coord, move: move) => {
@@ -30,34 +43,38 @@ let map_move_to_coords = (start_coord: coord, move: move) => {
   | Right =>
     let start = start_coord.x + 1;
     let stop = start_coord.x + move.distance;
-    Belt.Array.range(min(start, stop), max(start, stop))
-    ->Belt.List.fromArray
-    ->Belt.List.map(x => {x, y: start_coord.y});
+    range(start, stop)
+    ->Belt.List.mapWithIndex((idx, x) =>
+        {x, y: start_coord.y, steps: start_coord.steps + idx + 1}
+      );
   | Left =>
     let start = start_coord.x - 1;
     let stop = start_coord.x - move.distance;
-    Belt.Array.range(min(start, stop), max(start, stop))
-    ->Belt.List.fromArray
-    ->Belt.List.map(x => {x, y: start_coord.y});
+    range(start, stop)
+    ->Belt.List.mapWithIndex((idx, x) =>
+        {x, y: start_coord.y, steps: start_coord.steps + idx + 1}
+      );
   | Up =>
     let start = start_coord.y + 1;
     let stop = start_coord.y + move.distance;
-    Belt.Array.range(min(start, stop), max(start, stop))
-    ->Belt.List.fromArray
-    ->Belt.List.map(y => {x: start_coord.x, y});
+    range(start, stop)
+    ->Belt.List.mapWithIndex((idx, y) =>
+        {x: start_coord.x, y, steps: start_coord.steps + idx + 1}
+      );
   | Down =>
     let start = start_coord.y - 1;
     let stop = start_coord.y - move.distance;
-    Belt.Array.range(min(start, stop), max(start, stop))
-    ->Belt.List.fromArray
-    ->Belt.List.map(y => {x: start_coord.x, y});
+    range(start, stop)
+    ->Belt.List.mapWithIndex((idx, y) =>
+        {x: start_coord.x, y, steps: start_coord.steps + idx + 1}
+      );
   };
 };
 let map_move_list_to_coords = (moves: list(move)) => {
   let res: (coord, list(coord)) =
     Belt.List.reduce(
       moves,
-      ({x: 0, y: 0}, []),
+      ({x: 0, y: 0, steps: 0}, []),
       ((position: coord, coords: list(coord)), move) => {
         let new_coords = map_move_to_coords(position, move);
         let new_position = get_position_after_move(position, move);
@@ -97,50 +114,65 @@ module CoordMap =
   });
 
 /* The following test data has collision with manhattan distance 6 */
-/* let line1_input = "R8,U5,L5,D3"; */
-/* let line2_input = "U7,R6,D4,L4"; */
-let line1_input = Input3.line1_str;
-let line2_input = Input3.line2_str;
+let line1_input = "R8,U5,L5,D3";
+let line2_input = "U7,R6,D4,L4";
+/* let line1_input = Input3.line1_str; */
+/* let line2_input = Input3.line2_str; */
 let line1_tagged_coords =
   map_input_to_moves_list(line1_input)->map_move_list_to_coords->tag_coords_with_line_id(1);
 let line2_tagged_coords =
   map_input_to_moves_list(line2_input)->map_move_list_to_coords->tag_coords_with_line_id(2);
 let all_tagged_coords = Belt.List.concat(line1_tagged_coords, line2_tagged_coords);
 
-let does_collide = (querying_line_id, line_ids) => {
-  Belt.List.some(line_ids, line_id => line_id != querying_line_id);
+let does_collide = (querying_line_id: line_id, touches: touches) => {
+  Belt.List.some(touches, ((line_id, _)) => line_id != querying_line_id);
 };
-let (coord_map, collisions) =
+let (coord_map: CoordMap.t(touches), collision_touches) =
+  /* a "touch" is any time a line visit a coordinate */
   Belt.List.reduce(
     all_tagged_coords,
-    (CoordMap.empty, []),
+    (CoordMap.empty, []: list(touches)),
     (acc, (coord, line_id)) => {
-      let (coord_map, collisions) = acc;
+      let (coord_map, collision_touches) = acc;
       switch (CoordMap.find((coord.x, coord.y), coord_map)) {
       | exception Not_found =>
-        let new_map = CoordMap.add((coord.x, coord.y), [line_id], coord_map);
-        (new_map, collisions);
-      | line_ids =>
-        does_collide(line_id, line_ids)
-          ? {
-            let new_map = CoordMap.add((coord.x, coord.y), [line_id, ...line_ids], coord_map);
-            (new_map, [coord, ...collisions]);
-          }
-          : (coord_map, collisions)
+        let new_map = CoordMap.add((coord.x, coord.y), [(line_id, coord)], coord_map);
+        (new_map, collision_touches);
+      | touches when does_collide(line_id, touches) =>
+        let new_map =
+          CoordMap.add((coord.x, coord.y), [(line_id, coord), ...touches], coord_map);
+        (new_map, [[(line_id, coord), ...touches], ...collision_touches]);
+      | touches =>
+        let new_map =
+          CoordMap.add((coord.x, coord.y), [(line_id, coord), ...touches], coord_map);
+        (new_map, collision_touches);
       };
     },
   );
-let closest_collision =
-  Belt.List.map(collisions, collision_coord => {collision_coord.x + collision_coord.y})
+let closest_collision_by_manhattan_distance = {
+  Belt.List.map(collision_touches, touch => {Belt.List.reduce(touch)});
+  Belt.List.map(collision_touches, collision => {collision.x + collision.coord.y})
   ->Belt.List.sort((a, b) => a - b)
   ->Belt.List.head;
-let collision_output =
-  switch (closest_collision) {
+};
+let collision_output_manhattan_distance =
+  switch (closest_collision_by_manhattan_distance) {
   | Some(collision) => "Closest collision has manhattan distance: " ++ string_of_int(collision)
+  | None => "No collisions"
+  };
+
+let closest_collision_by_steps =
+  Belt.List.sort(collisions, (a, b) => compare(a.steps, b.steps))->Belt.List.head;
+let collision_output_steps =
+  switch (closest_collision_by_steps) {
+  | Some(collision) => "Closest collision by number of steps: " ++ string_of_int(collision.steps)
   | None => "No collisions"
   };
 
 [@react.component]
 let make = () => {
-  <div> collision_output->ReasonReact.string </div>;
+  <div>
+    <div> collision_output_steps->ReasonReact.string </div>
+    <div> collision_output_manhattan_distance->ReasonReact.string </div>
+  </div>;
 };
